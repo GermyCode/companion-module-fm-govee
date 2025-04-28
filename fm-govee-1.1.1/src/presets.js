f5b50cb3503e562fa1e3d4c1aa18ed2b7246dc11
GermyCode <ficman832@gmail.com>
2025-04-28 15:44:54 -0500

    adding commented default preset

--- fm-govee-1.1.1/src/presets.js
+++ fm-govee-1.1.1/src/presets.js
@@ -2,15 +2,41 @@ const { combineRgb } = require('@companion-module/base');
 
 module.exports = {
 	initPresets: function () {
+		/*
 		let self = this;
+		let presets = {}; 
 
-		let presets = [];
+		// Define colors
+		const foregroundColor = combineRgb(255, 255, 255); // White
+		const backgroundColorBlack = combineRgb(0, 0, 0);  // Black
 
-		const foregroundColor = combineRgb(255, 255, 255) // White
-		const foregroundColorBlack = combineRgb(0, 0, 0) // Black
-		const backgroundColorRed = combineRgb(255, 0, 0) // Red
-		const backgroundColorGreen = combineRgb(0, 255, 0) // Green
-		
-		self.setPresetDefinitions(presets);
+		presets.turn_on = {
+			type: 'button',
+			category: 'Lighting',
+			name: 'Turn On Light',
+			style: {
+				text: 'Turn On',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: backgroundColorBlack,
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'power',
+							options: {
+								power: 'on',
+							},
+						},
+					],
+					up: [],
+				},
+			],
+			feedbacks: []
+		};
+
+		//self.setPresetDefinitions(presets);
+		*/
 	}
-}
+};
